48e888e9008c7c088b9221d198bedb9a1b4237cf
5adiyah <sadiali@pdx.edu>
2016-02-05 23:11:42 -0800

    Add Static Site

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".button.broadway").click(function(){
+    $("body").removeClass();
+    $("body").css("background-color", "#E6E1D3");
+    $(".bridgeinfo").show();
+    $("#burnside").hide();
+  });
+
+
+});
